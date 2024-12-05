9f6f01ff460935f8ddf7382b03bf6e51c8492e75
ravi2krishna <ravi2krishna@gmail.com>
2024-12-05 04:04:07 +0000

    complete web app deploy script

--- script.sh
+++ script.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 echo "Deploying Web Apps On NGINX"
+
+echo "Updating System"
+sudo apt update -y
+
+echo "Install Utilities"
+sudo apt install -y zip unzip
+
+echo "Install NGINX"
+sudo apt install -y nginx
+
+echo "Remove Old Files"
+sudo rm -r /var/www/html
+
+echo "Deploy Login App"
+sudo git clone  https://github.com/ravi2krishna/login-2435.git /var/www/html
+
+echo "Deployed Web Apps On NGINX"
