ad4bf72b1201a021814b32173c2ea7eaae75579c
ravi2krishna <ravi2krishna@gmail.com>
2025-02-12 09:44:10 +0530

    create api subnet

--- tf/vpc.tf
+++ tf/vpc.tf
@@ -15,3 +15,12 @@ resource "aws_subnet" "lms-web-sn" {
     Name = "lms-web-subnet"
   }
 }
+
+resource "aws_subnet" "lms-api-sn" {
+  vpc_id     = aws_vpc.lms-vpc.id
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = "true"
+  tags = {
+    Name = "lms-api-subnet"
+  }
+}
