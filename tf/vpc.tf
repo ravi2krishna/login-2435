1acf3e5135d828045c8a9bba3559ab3be6ea691c
ravi2krishna <ravi2krishna@gmail.com>
2025-02-12 10:12:23 +0530

    create database secuirty group

--- tf/vpc.tf
+++ tf/vpc.tf
@@ -170,3 +170,29 @@ resource "aws_vpc_security_group_ingress_rule" "lms-api-sg-http" {
   ip_protocol       = "tcp"
   to_port           = 8080
 }
+
+resource "aws_security_group" "lms-db-sg" {
+  name        = "lms-db-sg"
+  description = "Allow DB Traffic"
+  vpc_id      = aws_vpc.lms-vpc.id
+
+  tags = {
+    Name = "lms-db-sg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "lms-db-sg-ssh" {
+  security_group_id = aws_security_group.lms-db-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "lms-db-sg-postgres" {
+  security_group_id = aws_security_group.lms-db-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 5432
+  ip_protocol       = "tcp"
+  to_port           = 5432
+}
