7b0d05acff326411d8962e0e78c3ea41b9f48f5b
ravi2krishna <ravi2krishna@gmail.com>
2025-02-15 09:21:27 +0530

    create lms api nsg ssh

--- tf/vnet.tf
+++ tf/vnet.tf
@@ -95,3 +95,17 @@ resource "azurerm_network_security_group" "lms-api-nsg" {
   location            = azurerm_resource_group.tf-rg.location
   resource_group_name = azurerm_resource_group.tf-rg.name
 }
+
+resource "azurerm_network_security_rule" "lms-api-nsg-ssh" {
+  name                        = "lms-api-ssh"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.tf-rg.name
+  network_security_group_name = azurerm_network_security_group.lms-api-nsg.name
+}
