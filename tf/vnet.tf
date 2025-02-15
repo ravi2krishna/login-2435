9cceed2005a1820378100a46841d515f0a1a9e12
ravi2krishna <ravi2krishna@gmail.com>
2025-02-15 08:54:22 +0530

    create api subnet

--- tf/vnet.tf
+++ tf/vnet.tf
@@ -19,3 +19,10 @@ resource "azurerm_subnet" "lms-web-sn" {
   virtual_network_name = azurerm_virtual_network.lms-vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
+resource "azurerm_subnet" "lms-api-sn" {
+  name                 = "lms-api-subnet"
+  resource_group_name  = azurerm_resource_group.tf-rg.name
+  virtual_network_name = azurerm_virtual_network.lms-vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
