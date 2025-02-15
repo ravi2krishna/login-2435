2472e07ec0a327fca9d435c48fadb56adc7285cf
ravi2krishna <ravi2krishna@gmail.com>
2025-02-15 09:17:04 +0530

    create lms web nsg http

--- tf/vnet.tf
+++ tf/vnet.tf
@@ -75,3 +75,17 @@ resource "azurerm_network_security_rule" "lms-web-nsg-ssh" {
   resource_group_name         = azurerm_resource_group.tf-rg.name
   network_security_group_name = azurerm_network_security_group.lms-web-nsg.name
 }
+
+resource "azurerm_network_security_rule" "lms-web-nsg-http" {
+  name                        = "lms-web-http"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.tf-rg.name
+  network_security_group_name = azurerm_network_security_group.lms-web-nsg.name
+}
